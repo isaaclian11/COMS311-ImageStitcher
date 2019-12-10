366df6033514fa3d67bdbe77f8e1a1735d758bd3
Justin <jmworley@iastate.edu>
2019-12-10 10:44:28 -0600

    added try-catch for to large of width reduction

--- pa2/ImageProcessor.java
+++ pa2/ImageProcessor.java
@@ -13,51 +13,57 @@ public class ImageProcessor {
     public static Picture reduceWidth(int x, String inputImage){
         Picture picture = new Picture(inputImage);
 
-
-        for(int c=0; c<x; c++) {
-            int m = picture.height();
-            int n = picture.width();
-            int[][] matrix = new int[m][n];
-            for (int j = 0; j < m ; j++) {
-                for (int i = 0; i < n; i++) {
-                    Color a = picture.get(i, j);
-                    Color b;
-                    if (i == 0) {
-                        b = picture.get(i + 1, j);
-                    } else if (i == n - 1) {
-                        b = picture.get(i-1, j );
-                    } else {
-                        a = picture.get(i-1, j);
-                        b = picture.get(i+1, j);
-                    }
-                    int dist = pixelDistance(a, b);
-                    matrix[j][i] = dist;
-                }
+        try {
+            if (x >= picture.width()) {
+               throw new ArrayIndexOutOfBoundsException();
             }
-            ArrayList<Tuple> tuples = MatrixCuts.widthCut(matrix);
-            Picture newPicture = new Picture(picture.width()-1, picture.height());
 
-            for(int r=0; r<picture.height(); r++){
-                for(int o=0; o<picture.width()-1; o++){
-                    if(0==tuples.get(r+1).getY()){
-                        newPicture.setRGB(o, r, picture.getRGB(o+1, r));
+            for (int c = 0; c < x; c++) {
+                int m = picture.height();
+                int n = picture.width();
+                int[][] matrix = new int[m][n];
+                for (int j = 0; j < m; j++) {
+                    for (int i = 0; i < n; i++) {
+                        Color a = picture.get(i, j);
+                        Color b;
+                        if (i == 0) {
+                            b = picture.get(i + 1, j);
+                        } else if (i == n - 1) {
+                            b = picture.get(i - 1, j);
+                        } else {
+                            a = picture.get(i - 1, j);
+                            b = picture.get(i + 1, j);
+                        }
+                        int dist = pixelDistance(a, b);
+                        matrix[j][i] = dist;
                     }
-                    else {
-                    newPicture.setRGB(o, r, picture.getRGB(o, r));
-                    }
-
                 }
+                ArrayList<Tuple> tuples = MatrixCuts.widthCut(matrix);
+                Picture newPicture = new Picture(picture.width() - 1, picture.height());
+
+                for (int r = 0; r < picture.height(); r++) {
+                    for (int o = 0; o < picture.width() - 1; o++) {
+                        if (0 == tuples.get(r + 1).getY()) {
+                            newPicture.setRGB(o, r, picture.getRGB(o + 1, r));
+                        } else {
+                            newPicture.setRGB(o, r, picture.getRGB(o, r));
+                        }
+
+                    }
+                }
+                picture = newPicture;
             }
-        picture = newPicture;
+        }catch (Exception e){
+
+            System.out.print("\nError - Trying to reduce width that is equal or greater than the Picture's width\n");
         }
-
         return picture;
-
     }
 
     public static void main(String[] args){
-        Picture p = reduceWidth(200, "na1.jpg");
-        p.save(new File("/Users/isaacsanga/Downloads/n1-result.jpg"));
+        Picture p = reduceWidth(1338, "na1.jpg");
+
+        p.save(new File("n1-result.jpg"));
     }
 
     public static int pixelDistance(Color c1, Color c2) {
